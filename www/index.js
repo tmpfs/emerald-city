303fca7ab2116776f5e531420b418fa0eff1b196
muji <muji@tmpfs.org>
2021-09-08 13:51:35 +0800

    Improve key handling mockup.

--- www/index.js
+++ www/index.js
@@ -1,12 +1,24 @@
 import Worker from 'worker-loader!./worker.js';
 
 const button = document.getElementById('keygen'),
+  thresholdInput = document.getElementById('threshold'),
+  partiesInput = document.getElementById('parties'),
+  partiesList = document.getElementById('parties-list'),
   payload = document.getElementById('payload'),
   progress = document.getElementById('progress'),
   label = document.getElementById('label'),
   data = document.getElementById('data'),
   json = document.getElementById('json'),
   complete = document.getElementById('complete');
+
+let actionType = 'keygen';
+let threshold = 1;
+let parties = 3;
+let actionData = { threshold, parties };
+let signingKeys = [];
+// Message must be Vec<u8>, do not use Uint8Array as that
+// gets serialized to a JSON object.
+const message = randomMessage();
 
 function show(el) {
   el.style.display = 'flex';
@@ -22,6 +34,14 @@ function randomMessage() {
   });
 }
 
+thresholdInput.addEventListener('change', (e) => {
+  threshold = Math.min(parseInt(e.target.value), parties - 1);
+});
+
+partiesInput.addEventListener('change', (e) => {
+  parties = parseInt(e.target.value);
+});
+
 function hex(bytes) {
   const chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
   return bytes.reduce((acc, u8) => {
@@ -30,11 +50,44 @@ function hex(bytes) {
   }, '');
 }
 
-let actionType = 'keygen';
-let actionData = {};
-// Message must be Vec<u8>, do not use Uint8Array as that
-// gets serialized to a JSON object.
-const message = randomMessage();
+function showPartyKeys(multiKey) {
+  show(partiesList);
+  const keys = multiKey[0];
+  const sharedKeys = multiKey[1];
+  const ge = multiKey[2];
+  const partyKeys = [];
+
+  for (let i = 0; i < parties;i++) {
+    partyKeys.push({
+      index: i,
+      key: keys[i],
+      sharedKey: keys[i],
+      ge: ge[i],
+    })
+  }
+
+  const tpl = document.getElementById('party-item');
+
+  for (const partyKey of partyKeys) {
+    const template = tpl.content.cloneNode(true);
+    const heading = template.querySelector('h3');
+    const button = template.querySelector('button');
+    heading.innerText = `Party ${partyKey.index + 1}`;
+    const pre = template.querySelector('details > pre');
+    pre.innerText = JSON.stringify(partyKey, undefined, 2);
+
+    button.addEventListener('click', (e) => {
+      signingKeys.push(partyKey);
+      if (signingKeys.length === threshold + 1) {
+        console.log('Got enough signing keys to proceed...');
+      }
+
+      e.currentTarget.setAttribute('disabled', '1');
+    });
+
+    partiesList.appendChild(template);
+  }
+}
 
 if (window.Worker) {
   const worker = new Worker('worker.js');
@@ -57,14 +110,13 @@ if (window.Worker) {
       });
     } else if (e.data.type === 'keygen_done') {
       hide(progress);
-      // Key generation is completed
-      show(data);
-      json.innerText = JSON.stringify(e.data.keys, undefined, 2);
+
+      showPartyKeys(e.data.keys);
 
       // Prepare for next phase
       button.innerText = 'Sign message';
       actionType = 'sign_message';
-      actionData = {message};
+      actionData = {...actionData, message};
 
     } else if (e.data.type === 'sign_message_done') {
       hide(progress);
--- www/worker.js
+++ www/worker.js
@@ -1,24 +1,22 @@
 // NOTE: must be an asynchronous import
 import('emerald-city')
   .then((wasm) => {
-    const signThreshold = 2;
-    const numParties = 3;
-    const ttag = 3;
-
     // Store WASM response values here for now
     let keys, signed;
 
     onmessage = (e) => {
       const {data} = e;
       if (data.type === 'keygen') {
-        keys = wasm.keygen(signThreshold, numParties);
+        const {threshold, parties} = data;
+        keys = wasm.keygen(threshold, parties);
         postMessage({type: 'keygen_done', keys});
       } else if (data.type === 'sign_message') {
-        const {message} = data;
-        signed = wasm.sign_message(signThreshold, ttag, keys, message);
+        const {message, threshold, parties} = data;
+        signed = wasm.sign_message(threshold, parties, keys, message);
         postMessage({type: 'sign_message_done', signed});
       } else if (data.type === 'verify_signature') {
-        wasm.verify_signature(ttag, signed);
+        const {parties} = data;
+        wasm.verify_signature(parties, signed);
         postMessage({type: 'verify_signature_done', keys});
       }
     }
